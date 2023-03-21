ed65c8a72cdc797b22c01f4b60e2c9bfbe89a4ec
Emmet1 <78420136+Emmet1@users.noreply.github.com>
2023-03-21 04:58:43 +0000

    Update deploy.sh
    changes to environment variables in circles

--- deploy.sh
+++ deploy.sh
@@ -4,5 +4,7 @@ sudo npm install -g pm2
 pm2 stop simple_app
 cd SimpleApplication/
 npm install 
+echo $PRIVATE_KEY > privatekey.pem
+echo $SERVER > server.crt
 pm2 start ./bin/www --name simple_app
 
